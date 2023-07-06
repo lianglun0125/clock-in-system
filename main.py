b62b828ff759ab7f581cd77a19399e3550302df0
lzl0125 <lianglun0125@gmail.com>
2023-07-06 11:44:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import getpass
 import sys
 from threading import Thread
 
+
 # Version 1.3.0
 
 # 檢查資料夾是否存在，如果不存在，則創建資料夾
@@ -257,39 +258,65 @@ def show_history():
 
     root.update()
 
+def show_version():
+    version_w = tk.Toplevel(root)
+    version_w.title('版本資訊')
+    version_w.geometry("218x120")
+    version_w.resizable(False, False)
+
+    version_label = tk.Label(version_w, text='Ver. 1.3.0 更新內容', font=('微軟正黑體', 10, 'bold'), fg='#191970')
+    version_label.pack()
+    new_label = tk.Label(version_w, text='> 匯出簽到記錄造成時間錯誤修正', font=('微軟正黑體', 10, 'bold'), fg='#191970')
+    new_label.pack()
+    new1_label = tk.Label(version_w, text='> 匯出簽到記錄新增[工時]', font=('微軟正黑體', 10, 'bold'), fg='#191970')
+    new1_label.pack()
+    new2_label = tk.Label(version_w, text='> 新增[檢查更新]按鈕', font=('微軟正黑體', 10, 'bold'), fg='#191970')
+    new2_label.pack()
+    update_label = tk.Label(version_w, text='Last Update - 20230706', font=('微軟正黑體', 10, 'bold'), fg='#191970')
+    update_label.pack()
+
+def create_menu_bar(root):
+    menu_bar = tk.Menu(root)
+    record_menu = tk.Menu(menu_bar, tearoff=0)
+    help_menu = tk.Menu(menu_bar, tearoff=0)
+
+    record_menu.add_command(label="查看歷史紀錄", command=show_history)
+    record_menu.add_command(label="匯出簽到紀錄", command=export_records)
+    help_menu.add_command(label="檢查更新", command=check_update)
+    help_menu.add_command(label="版本資訊", command=show_version)
+
+    menu_bar.add_cascade(label="簽到記錄", menu=record_menu)
+    menu_bar.add_cascade(label="幫助", menu=help_menu)
+    root.config(menu=menu_bar)
+
 root = tk.Tk()
 root.title('1.3.0')
 root.geometry("205x165")
 root.resizable(False, False)
 
+create_menu_bar(root)
+
 signin_button = ttk.Button(root, text='今日簽到', command=lambda: save_record('簽到'))
-signin_button.pack()
+signin_button.place(x=60,y=30)
 
 signout_button = ttk.Button(root, text='今日簽退', command=lambda: save_record('簽退'))
-signout_button.pack()
+signout_button.place(x=60,y=70)
 
-history_button = ttk.Button(root, text='查看簽到歷史紀錄', command=show_history)
-history_button.pack()
-
-export_records_button = ttk.Button(root, text='匯出簽到紀錄', command=export_records)
-export_records_button.pack()
-
-check_update_button = ttk.Button(root, text='檢查更新', command=check_update)
-check_update_button.pack()
-
-piglabel = tk.Label(root, text='阿咪今天簽到了嗎？', font=('微軟正黑體', 10, 'bold'), fg='#191970')
-piglabel.pack()
-
-updatelabel = tk.Label(root, text='Last Update - 20230706', font=('微軟正黑體', 10, 'bold'), fg='#191970')
-updatelabel.pack()
+piglabel = tk.Label(root, text='阿咪今天簽到了嗎？', font=('微軟正黑體', 12, 'bold'), fg='#191970')
+piglabel.place(x=32,y=110)
 
 exe_name = os.path.basename(sys.argv[0])
 if exe_name == "Clock-In-NEW.exe":
-        new_name = "Clock-In.exe"
-        if os.path.exists("Clock-In.exe"):
-            os.remove("Clock-In.exe")
-        os.rename(exe_name, new_name)
-        messagebox.showinfo('提示', '首次執行此版本，環境設置成功，請使用管理員身分重新啟動。')
-        sys.exit()
+    new_name = "Clock-In.exe"
+    if os.path.exists("Clock-In.exe"):
+        os.remove("Clock-In.exe")
+    os.rename(exe_name, new_name)
+    messagebox.showinfo('提示', '首次執行此版本，環境設置成功，請使用管理員身分重新啟動。')
+    sys.exit()
 else:
-        root.mainloop()
+    root.mainloop()
+
+
+
+
+
